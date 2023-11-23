4cfa3b05af0ec8d501bd74a395512f717637cc72
Samuel Charlier <samuel.charlier@student.hepl.be>
2023-11-23 17:09:18 +0100

    fonction SendSimple ok

--- src/model/MailReceive.java
+++ src/model/MailReceive.java
@@ -1,4 +1,12 @@
 package model;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+//import javax.activation.*;
+import java.util.*;
 public class MailReceive {
+    private Session sess;
+
+
+
 }
--- src/model/SendSimple.java
+++ src/model/SendSimple.java
@@ -1,6 +1,48 @@
 package model;
 
+import javax.mail.*;
+import javax.mail.internet.*;
+//import javax.activation.*;
+import java.util.*;
 
 
 public class SendSimple {
+
+    private Session sess;
+
+    public SendSimple(Session s)
+    {
+        sess = s;
+    }
+
+    public void SendMailSimple(String de, String vers, String titre, String text){
+        try
+        {
+            System.out.println("Création du message");
+            String exp = de;
+            String dest = vers;
+            String sujet = titre;
+            String texte = text;
+            MimeMessage msg = new MimeMessage (sess);
+            msg.setFrom (new InternetAddress (exp));
+            msg.setRecipient (Message.RecipientType.TO, new InternetAddress (dest));
+            msg.setSubject(sujet);
+            msg.setText (texte);
+
+            System.out.println("Envoi du message");
+            Transport.send(msg);
+            System.out.println("Message envoyé");
+        }
+        catch (MessagingException e)
+        {
+            System.out.println("Errreur sur message : " + e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Errreur sur message : " + e.getMessage());
+        }
+
+    }
+
+
 }
