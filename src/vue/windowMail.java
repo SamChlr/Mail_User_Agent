c07ce5c117a13aa48c51deab97ed3edd8b517460
OrvalVie <vincent.brulet@student.hepl.be>
2023-11-25 00:11:00 +0100

    Début interface graphique envoie de mail

---
+++ src/vue/windowMail.java
@@ -0,0 +1,9 @@
+package vue;
+
+import javax.swing.*;
+
+public class windowMail {
+    private JPanel JPanelFenetre;
+    private JButton nouveauMailButton;
+    private JTable table1;
+}
---
+++ src/vue/windowNouveauMail.java
@@ -0,0 +1,12 @@
+package vue;
+
+import javax.swing.*;
+
+public class windowNouveauMail {
+    private JPanel JPanelNouveauMail;
+    private JTextField dest_textField;
+    private JTextField objet_textField;
+    private JButton ajouterPièceJointeButton;
+    private JTextArea mail_textArea;
+    private JLabel Nom_JLabel;
+}
